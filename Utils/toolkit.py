36abb786c987e4f5673f51f29420d492b9de466f
kuro <im.kuro.business@gmail.com>
2023-07-27 11:37:09 -0400

    working push | fixed like, fucking everything

--- Utils/toolkit.py
+++ Utils/toolkit.py
@@ -1,58 +1,191 @@
-import json
+#local imports
+from . import helpers
+
+import json, spacy, re, requests, random, time
 
 
 class tools():
 	def __init__(self, clientObj: dict) -> None:
 		self.clientObj = clientObj    
-    
+		self.helpersObj = helpers.IOFuncs()
+		self.reqSession = requests.Session()
+	
 
-    # INSTA STUFF
 	def getAccountInfo(self, username: str) -> dict:
 		return self.clientObj.user_info_by_username(username)
-		
-  
-  
-	def gatherFollowers(self, username):
-		self.clientObj.user_followers(username)
-
-
-	def gatherFollowing(self, username):
-		# Gather following
-		pass
-
-	def gatherPosts(self, username):
-		# Gather posts
-		pass
-
-
-	def gatherLikedBy(self, ):
-		# Gather posts liked by users
-		pass
-
-
-
-
+	def followUser(self, pk: str) -> bool: 
+		return self.clientObj.user_follow(pk)
 
 	# MISC STUFF
  
 	def saveToFile(self, path: str, data) -> bool:
+
 		try:
 			# Save data to file
-			jsonDBObj = json.loads(open("sessions.json").read())
+			jsonDBObj = json.loads(open("Utils/sessions.json").read())
 			jsonDBObj[path] = data
-			# write changes to master database
+			# write changes to database
 			with open("Utils/sessions.json", "w") as masterDatabaseFile:
 				masterDatabaseFile.write(json.dumps(jsonDBObj, indent=4))
 			return True
 		except Exception as e:
 			return e
 
-	def sessionSetup(self):
-		pass
+
+  
+	def __fix__(self) -> bool:
+		"""This can be ran by runningn with -f to fix any possible issues with the tool
+
+		Returns:
+			bool: _description_
+		"""
+		try:
+			# Save data to file
+			jsonDBObj = json.loads(open("Utils/sessions.json").read())
+			jsonDBObj["username"] = ""
+			jsonDBObj["settingsDict"] = {}
+			# write changes to database
+			with open("Utils/sessions.json", "w") as masterDatabaseFile:
+				masterDatabaseFile.write(json.dumps(jsonDBObj, indent=4))
+			return True
+		except Exception as e:
+			return e
 
 
+
+
+	def sessionSetup(self)-> bool:
+		"""Used on startup to check if we have a session saved, if not we get account info and save it to file
+
+		Returns:
+			bool: worked or not
+		"""
+		# check if we have a session saved | if not, get account info | if so load session
+		if json.loads(open("Utils/sessions.json").read())["settingsDict"] == {}:
+			# get account info
+			ACCOUNT_USERNAME = self.helpersObj.Default.getTextInput("Enter your username")
+			ACCOUNT_PASSWORD = self.helpersObj.Default.getPassword("Enter your password")
+			# log in
+			self.clientObj.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+			self.helpersObj.Default.printSuccess(f"Login success!\n")
+
+			if self.helpersObj.Default.getUserInput("Do you want to save your session (you can login without needing to input your user & pass)").lower() == "y":
+				# Save the settings session to login faster in the future
+				if self.saveToFile("settingsDict", self.clientObj.get_settings()) and self.saveToFile("username", ACCOUNT_USERNAME) and self.saveToFile("cookieDict", self.clientObj.cookie_dict) == True:			
+					self.helpersObj.Default.printInfo("Saved settings to file!")
+		
+				else: 
+					self.helpersObj.Default.printError(f"Failed to save settings to file!")
+					return False
+			else: 
+				self.helpersObj.Default.printInfo("Not saving settings to file!")
+				return True
+		# else load session to login 
+		else: 
+			# load session
+			self.clientObj.set_settings(json.loads(open("Utils/sessions.json").read())["settingsDict"])
+			self.helpersObj.Default.printSuccess(f"loaded session!\n")
+			return True
 
 
 	def TFAHandler(self):
 		pass
 		# https://github.com/adw0rd/instagrapi - > additional ex.
+  
+ 
+	# Main funcs
+ 
+
+	def crossRefrenceAccounts(self, targetPK: str) -> json:
+		"""This will get followers & following to return the target's mutuals
+
+		Args:
+			targetPK (str): target insta ID
+
+		Returns:
+			json: json of the target's mutuals
+		"""
+		self.helpersObj.Default.printInfo(f"Please be patient, if the target has a lot of followers/following this could take 1-5 mins.")
+		# get followers and following based on the count, we do this to lower api calls (hopefully idk lol)
+		userFollowers = self.clientObj.user_followers_gql_chunk(targetPK)
+		time.sleep(random.randint(30, 45))
+		userFollowing = self.clientObj.user_following_v1(targetPK)
+		
+		jsonRes = {}
+		# Assuming you have a list of users being followed stored in 'userFollowing'
+		# Replace 'userFollowing' with the actual variable name containing the list of users being followed.
+		userFollowing_usernames = [user.username for user in userFollowing]
+
+		# Check if the target is following any of their followers
+		for follower in userFollowers[0]:
+			if follower.username in userFollowing_usernames:
+				jsonRes[follower.username] = {"isMutuals": True}
+				self.helpersObj.Default.printSuccess(f"{follower.username} is friends with the target")
+			
+		return jsonRes
+
+
+	def crossRefrencePostsLikers(self) -> json:
+		pass
+
+
+
+	def pharseBios(self, userBio: str) -> json:
+		
+		nlp = spacy.load("en_core_web_sm")
+
+		# Process the bio text using spaCy
+		doc = nlp(userBio)
+		
+		age_pattern = r'\b\d{1,2}\b'  # Matches numbers with 1 or 2 digits
+
+		# Initialize lists to store extracted entities
+		counties = []
+		abbreviations = []
+		schools = []
+		
+		# Iterate through the entities identified by spaCy
+		for ent in doc.ents:
+			if ent.label_ == "GPE":  # Geo-Political Entity (e.g., countries, cities, states)
+				counties.append(ent.text)
+			elif ent.label_ == "ORG":  # Organizations
+				schools.append(ent.text)
+			elif ent.label_ == "LOC":  # Locations
+				abbreviations.append(ent.text)
+    
+		ages = re.findall(age_pattern, userBio)
+  
+		return counties, abbreviations, schools, ages
+
+
+
+
+	def handlePrivateStatus(self, tarInfo: str) -> bool:
+		"""Handles a target private acc
+
+		Args:
+			tarInfo (str): target info
+
+		Returns:
+			bool: worked or not
+		"""
+		if tarInfo.is_private == True:
+			userInput = self.helpersObj.Default.getUserInput("This account is private. Do you want to request to follow them? (if you have already followed them and they accepted press ENTER.)").lower()
+			if userInput == "y":
+				self.toolkitObj.followUser(tarInfo.pk)
+				self.helpersObj.Default.printError(f"Attempted to follow {tarInfo.username}")
+				self.helpersObj.Default.printInfo(f"Come back later when they accept :(")
+				exit(0)
+			elif userInput == "":
+				return True
+			else:  
+				self.helpersObj.Default.printInfo(f"Please choose an account thats not private.")
+				exit(0)
+		
+
+
+
+
+
+
+
--- run.py
+++ run.py
@@ -3,31 +3,45 @@
 import Utils.toolkit as toolkit, Utils.helpers as helpers
 # non local
 from instagrapi import Client, exceptions
-import requests, json, os
+import requests, os, time, random, argparse
 
+argParser = argparse.ArgumentParser(description='Instagram cross refrence tool')
+
+argParser.add_argument('-f', '--fix', help='fix any issues with the session, if you run into any API errors or somthing thats isnt handled in the code you can run with -f/--fix to fix any possible issues', action='store_true')
+
+parsedArgObj = argParser.parse_args()
 
 def main(tarUsername: str) -> bool:
 	helpersObj.Default.printInfo(f"Gathering target account info => {tarUsername}")
-	
+
 	# get target account info (dict)
 	tarInfo = toolkitObj.getAccountInfo(tarUsername)
 	# print target account info
 	helpersObj.Default.printArgsInfo(Target_Username=tarInfo.username, Target_PK=tarInfo.pk, Target_Followers=tarInfo.follower_count, Target_Following=tarInfo.following_count)
 
-	# start extracting data (followers/following/posts likers)
-
-
-
-
-
-	# compare data to see if the account is real or not
+	# handle target account status
+	toolkitObj.handlePrivateStatus(tarInfo)
  
+ 	# sleep to avoid ratelimit
+	time.sleep(random.randint(10, 30))
 
+	# find mutals
+	mutuals = toolkitObj.crossRefrenceAccounts(tarInfo.pk)
+	exit(0)
+ 
+	time.sleep(random.randint(15, 30))
+	tarPosts = toolkitObj.gatherPosts(tarInfo.pk)
+	print(tarPosts)
+
+	time.sleep(random.randint(15, 30))
+
+	
+	# compare data to see if the account is real or not
 
 
 	# give rating on how real the account is
- 
- 
+	helpersObj.Default.printSuccess(f"Target account rating: {accsRes + postsRes}")
+
 
 
 
@@ -40,9 +54,6 @@ if __name__ == '__main__':
 		os.system('cls')
 	else:
 		os.system('clear')
-
-	######## objects #########
-	##########################
 	# init wrapper
 	clientObj = Client()
     # mainly for input/output
@@ -50,47 +61,37 @@ if __name__ == '__main__':
 	# i init the toolkit here because we need the account obj to be passed in (yes im sure theres a better way to do this)
 	toolkitObj = toolkit.tools(clientObj)
 
-	
 	# banner and other bullshit
-	print(helpersObj.Default.banner)
+	for _ in helpersObj.Default.banner.splitlines():
+		print(_)
+		time.sleep(0.1)
 	helpersObj.Default.printInfo("Welcome to Kuro's Instagram cross refrence tool!")
-	helpersObj.Default.printInfo("This tool will allow you to cross refrence a target's followers, following, and posts to see how real an account is.\n\n")
+	helpersObj.Default.printInfo("This tool will allow you to cross refrence a target's followers, following, and posts to see how real an account is")
+
+	# check if sessions need to be fixed
+	if parsedArgObj.fix:
+		helpersObj.Default.printInfo("Fixing session...")
+		if toolkitObj.__fix__():
+			helpersObj.Default.printSuccess("Session fixed! You can now log in again.")
+		else: helpersObj.Default.printError(f"Session error! Unable to fix session.")
+		
 
  
  	# get target username
 	target = helpersObj.Default.getTextInput("Enter the target username you want info on")
 
-	# check if we have a session saved | if not, get account info | 
-	if json.loads(open("sessions.json").read())["settingsDict"] == {}:
-		# get account info
-		ACCOUNT_USERNAME = helpersObj.Default.getTextInput("Enter your username")
-		ACCOUNT_PASSWORD = helpersObj.Default.getPassword("Enter your password")
-		# log in
-		clientObj.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
-  
-  
-		if helpersObj.Default.getUserInput("Do you want to save your session (you can login without needing to input your user & pass)").lower() == "y":
-			# Save the settings session to login faster in the future
-			if toolkitObj.saveToFile("settingsDict", clientObj.settings) == True:
-				helpersObj.Default.printInfo("Saved settings to file!")
-			else: helpersObj.Default.printError(f"Failed to save settings to file! Error: {toolkitObj.saveToFile('settingsDict', clientObj.settings())}")
-		else: helpersObj.Default.printInfo("Not saving settings to file!")
-	# else load session to login 
-	else: 
-		# load session
-		clientObj.set_settings(json.loads(open("sessions.json").read())["settingsDict"])
+	try:  
+		# check if we have a session saved | if not, get account info | if so load session
+		toolkitObj.sessionSetup()
 	
-
-	try:
 		# start main func
 		main(target)
 	except exceptions.BadPassword: helpersObj.Default.printError("Bad password!")
 	except exceptions.ClientNotFoundError: helpersObj.Default.printError("The username used to login was not found!")
 	except exceptions.ChallengeRequired: helpersObj.Default.printError("Challenge error! Please log in o the web and try again.")
 	except requests.exceptions.HTTPError: helpersObj.Default.printError("Request error! Could be any issue with the request. Please log in on the web to see any possible issues.")
-
-
-
+	except exceptions.ChallengeUnknownStep: helpersObj.Default.printError("Challenge error! Please log in on the web and try again.")
+	except Exception as e: helpersObj.Default.printError(f"Unknown error! ERROR: {e}")
 
 
 
